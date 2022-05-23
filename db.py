830d2a0e8e22d9484d73ebe1351d706ab35babfb
Masachusets <apofeoz@tut.by>
2022-05-23 17:28:09 +0300

    first commit

---
+++ db.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from pprint import pprint
+
+db = 'postgresql://dima:1242@localhost:5432/music'
+engine = sqlalchemy.create_engine(db)
+conn = engine.connect()
+
+sel = conn.execute(
+"""
+SELECT * FROM genres;
+""").fetchall()
+pprint(sel)
